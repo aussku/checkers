d119bf4b89c4eed9f26491d590fe3fda83e6b368
Simonas Bagdonavičius <simonasbagdo@gmail.com>
2026-02-21 19:46:50 +0200

    Added simple buttons and a header

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
+// Serve static files from frontend folder
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(PORT, () => {
